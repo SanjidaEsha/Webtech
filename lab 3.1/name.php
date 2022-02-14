47a209a952958d95100ac369a72c65ca9edbbabb
SanjidaEsha <98301602+SanjidaEsha@users.noreply.github.com>
2022-02-14 13:58:54 +0600

    Update name.php

--- lab 3.1/name.php
+++ lab 3.1/name.php
@@ -1,1 +1,31 @@
 
+<!DOCTYPE html>
+<html>
+
+<body>
+	<!-- Heading -->
+	<h3> HTML input form </h3>
+
+	<!-- HTML form -->
+	<form method="POST">
+		<h4> Name : </h4>
+		<input type="text" name="name"><br>
+	
+
+		<input type="submit" name="submit">
+	</form>
+</body>
+
+</html>
+<?php
+	
+	
+	if (isset($_POST['submit'])) {
+	
+		
+	$name = $_POST['name'];
+		
+
+		echo "<h1><i>  $name  </i></h1>";
+	}
+?>
